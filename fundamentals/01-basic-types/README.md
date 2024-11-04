2d1d568716c058aab73faa58b1e4b153836e1535
ebagabee <2001.gabrielhas@gmail.com>
2024-11-04 08:07:04 -0300

    chore: initial project

---
+++ fundamentals/01-basic-types/README.md
@@ -0,0 +1,13 @@
+# TypeScript Learning Journey
+
+## Module 1: Fundamentals
+
+### Exercise 1: User Management System
+
+This exercise focuses on implementing a basic user management system using TypeScript's fundamental concepts:
+
+- Basic Types
+- Interfaces
+- Classes
+- Array manipulation
+- Type validation
---
+++ fundamentals/01-basic-types/src/index.ts
@@ -0,0 +1,22 @@
+// Basic types
+let firstname: string = "João";
+let age: number = 25;
+let active: boolean = true;
+
+// type inference
+let lastname = "Silva";
+let number = 42;
+
+// Arrays
+let numbers: number[] = [1, 2, 3];
+let names: Array<string> = ["Ana", "Pedro"];
+
+// Tuple
+let coordinates: [number, number] = [10, 20];
+
+// Enum
+enum StatusUser {
+  Active = "ACTIVE",
+  Inative = "INACTIVE",
+  Pending = "PENDING",
+}
